407b62e1aaab8ee6be1ee46bddc8bc47b8f43b5d
chenpakman <chen36361@gmail.com>
2023-04-29 20:49:04 +0300

    design

--- app/src/main/java/com/example/emlody/Activities/AnalyzeEmotionActivity.java
+++ app/src/main/java/com/example/emlody/Activities/AnalyzeEmotionActivity.java
@@ -201,7 +201,8 @@ private void checkCameraPermissionsAndOpenCamera(){
                 .build();
 
         Request request = new Request.Builder()
-                .url("http://192.168.1.218:9000/app?emotions=" + emotions)
+               // .url("http://192.168.1.218:9000/app?emotions=" + emotions)
+                .url("http://192.168.1.34:9000/app?emotions=" + emotions)
                 .put(new RequestBody() {
                     @Override
                     public MediaType contentType() {
